b19dc341d6ca1c284850c95f9e8cf70fea3f72d7
Lucas Zheng <baiulus39@gmail.com>
2026-04-15 10:41:40 -0400

    skib

---
+++ app/templates/profile.js
@@ -0,0 +1,12 @@
+import Chart from 'chart.js/auto'
+
+(async function() {
+  const names = []
+  const totalVideos = []
+  const totalSubscribers = []
+  const totalViews = []
+  const totalLikes = []
+  const totalCommments = []
+  const averageViews = []
+  const averageLikes = []
+}
